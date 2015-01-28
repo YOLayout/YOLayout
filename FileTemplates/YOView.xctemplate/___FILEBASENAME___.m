48627c5203de912db9439163e320c642efd141b8
John Boiles <johnaboiles@gmail.com>
2015-01-27 17:57:30 -0800

    YOView file template

---
+++ FileTemplates/YOView.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,23 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)viewInit {
+    __weak typeof(self) weakSelf = self;
+    self.layout = [YOLayout layoutWithLayoutBlock:^CGSize(id<YOLayout> layout, CGSize size) {
+        return size;
+    }];
+}
+
+@end
