65d951d36279ac550d71f172e7d2cbc05837e189
John Boiles <johnaboiles@gmail.com>
2015-01-30 21:21:09 -0800

    More templates

---
+++ FileTemplates/UIViewController+YOView.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <YOLayout/YOView.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : YOView
+
+@end
---
+++ FileTemplates/UIViewController+YOView.xctemplate/___FILEBASENAME___Controller.m
@@ -0,0 +1,23 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___Controller.h"
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___Controller ()
+@property ___FILEBASENAMEASIDENTIFIER___ *contentView;
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___Controller
+
+- (void)loadView {
+    self.contentView = [[___FILEBASENAMEASIDENTIFIER___ alloc] init];
+    self.view = self.contentView;
+}
+
+@end
